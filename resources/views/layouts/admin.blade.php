74f2bf81726a0c377ce8772966fd6ad49f1c0310
Satyanr <satyanr84@gmail.com>
2024-09-17 14:29:56 +0700

    disable manual book

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -401,7 +401,7 @@
 
     <x-admin.footer />
 
-    <!-- Manual Book Modal -->
+    {{-- <!-- Manual Book Modal -->
     <div class="modal fade" id="BookModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -410,12 +410,12 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <iframe src="https://drive.google.com/file/d/175jzEHLCS9Xl-5Ot2hNvsuCnzh-mcGu1/view?usp=drive_link"
-                        frameborder="0"></iframe>
+                    <iframe src="{{asset("manual/Manual-User.pdf")}}"
+                        frameborder="0" width="100%" height="300px"></iframe>
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
